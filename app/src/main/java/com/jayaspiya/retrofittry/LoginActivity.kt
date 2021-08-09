0b2acd30a9c84b45c05e174e6766c5530b01d935
Jayas Piya <pzayazz@gmail.com>
2021-08-09 11:24:54 +0545

    :sparkles: Added Login Feature

--- app/src/main/java/com/jayaspiya/retrofittry/LoginActivity.kt
+++ app/src/main/java/com/jayaspiya/retrofittry/LoginActivity.kt
@@ -3,11 +3,14 @@ package com.jayaspiya.retrofittry
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import com.google.android.material.textfield.TextInputEditText
+import com.jayaspiya.retrofittry.model.User
+import com.jayaspiya.retrofittry.repo.UserRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var etUsername: TextInputEditText
@@ -34,6 +37,28 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun login() {
-        TODO("Not yet implemented")
+        val user = User(username = etUsername.text.toString(), password = etPassword.text.toString())
+        CoroutineScope(Dispatchers.IO).launch {
+            try{
+                val repo = UserRepo()
+                val response = repo.loginUser(user)
+                if(response.success == true){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@LoginActivity, "User Login", Toast.LENGTH_SHORT).show()
+                    }
+                    startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                }
+                else{
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@LoginActivity, "Invalid Credential", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            catch(ex:ExceptionInInitializerError){
+                withContext(Dispatchers.Main){
+                    Toast.makeText(this@LoginActivity, "error $ex", Toast.LENGTH_SHORT).show()
+                }  
+            }
+        }
     }
 }
